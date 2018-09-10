9941e2c68f2df423468f3fff86b66bbba25e0d11
Cristóvão Amado <cristovao.amado@gmail.com>
2018-09-10 16:09:03 +0100

    startup update

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -204,15 +204,15 @@ namespace DatingApp.API
                 );
             });
 
-            // app.Use(async (context, next) =>
-            // {
-            //     await next();
-            //     if (context.Response.StatusCode == 404 && !Path.HasExtension(context.Request.Path.Value))
-            //     {
-            //         context.Request.Path = "/index.html";
-            //         await next();
-            //     }
-            // });
+            app.Use(async (context, next) =>
+            {
+                await next();
+                if (context.Response.StatusCode == 404 && !Path.HasExtension(context.Request.Path.Value))
+                {
+                    context.Request.Path = "/index.html";
+                    await next();
+                }
+            });
 
         }
     }
